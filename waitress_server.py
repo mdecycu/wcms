3b307439321509eb539897bf4277a7735456feb8
unknown <mdecycuorg@gmail.com>
2022-10-01 20:52:39 +0800

    update 抽點 from 69

---
+++ waitress_server.py
@@ -0,0 +1,7 @@
+# pip install waitress
+from waitress import serve
+
+from cmsimde import flaskapp
+
+# run cmsimde dynamic site with production waitress
+serve(flaskapp.app, host='127.0.0.1', port=5006, url_scheme='https')
