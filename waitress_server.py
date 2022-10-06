deb1c450765606a4ba9add9cca3fe9b246012eda
unknown <mdecycuorg@gmail.com>
2022-10-06 15:13:42 +0800

    加入 w5 筆記

--- waitress_server.py
+++ waitress_server.py
@@ -4,4 +4,4 @@ from waitress import serve
 from cmsimde import flaskapp
 
 # run cmsimde dynamic site with production waitress
-serve(flaskapp.app, host='127.0.0.1', port=5006, url_scheme='https')
+serve(flaskapp.app, host='127.0.0.1', port=5006, url_scheme='https', threads=16)
